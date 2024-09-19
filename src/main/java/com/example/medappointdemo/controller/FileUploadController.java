7e35b97276b7e5708d89e43dfb59ec0c640563e5
FFinJA <161335409+FFinJA@users.noreply.github.com>
2024-09-19 18:01:20 -0400

    Hard coding the dbname.

--- src/main/java/com/example/medappointdemo/controller/FileUploadController.java
+++ src/main/java/com/example/medappointdemo/controller/FileUploadController.java
@@ -27,8 +27,8 @@ public class FileUploadController {
 //    @Value("${upload.path}")
 //    private String uploadDirectory;
 
-    @Value("${S3_BUCKET_NAME}")
-    String bucketName;
+    // @Value("${S3_BUCKET_NAME}")
+    String bucketName = "myherokubucket";
 
     @Autowired
     private AmazonS3 amazonS3;
