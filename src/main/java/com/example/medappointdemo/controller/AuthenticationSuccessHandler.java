cbd72845b78daa641403434f67b67d1bab2c1452
FFinJA <161335409+FFinJA@users.noreply.github.com>
2024-09-22 09:27:26 -0400

    Fix the login logic.

--- src/main/java/com/example/medappointdemo/controller/AuthenticationSuccessHandler.java
+++ src/main/java/com/example/medappointdemo/controller/AuthenticationSuccessHandler.java
@@ -46,11 +46,12 @@ public class AuthenticationSuccessHandler extends SavedRequestAwareAuthenticatio
         request.getSession().setAttribute("firstName", user.getFirstName());
         request.getSession().setAttribute("lastName", user.getLastName());
 
-        String continueUrl = request.getParameter("continue");
-        if (continueUrl != null && !continueUrl.isEmpty()) {
-            // Redirect to the continue URL if present
-            setDefaultTargetUrl(continueUrl);
-        } else if (authentication.getAuthorities().stream()
+//        String continueUrl = request.getParameter("continue");
+//        if (continueUrl != null && !continueUrl.isEmpty()) {
+//            // Redirect to the continue URL if present
+//            setDefaultTargetUrl(continueUrl);
+//        } else
+        if (authentication.getAuthorities().stream()
             .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"))) {
             setDefaultTargetUrl("/admins/");
         } else if (authentication.getAuthorities().stream()
